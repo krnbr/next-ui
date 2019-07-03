67c4e1ca93824128868984840c0291892977fa35
Karanbir Singh <krnbr@live.in>
2019-07-03 17:24:52 +0530

    added posts dynamic

--- pages/post.js
+++ pages/post.js
@@ -1,11 +1,20 @@
 import { withRouter } from 'next/router';
 import Layout from '../components/MyLayout.js';
 
-const Page = withRouter(props => (
-    <Layout>
+const Content = withRouter(props => (
+    <div>
+        {
+            console.log('query --> '+props.router.query.title)
+        }
         <h1>{props.router.query.title}</h1>
         <p>This is the blog post content.</p>
-    </Layout>
+    </div>
 ));
+
+const Page = props => (
+    <Layout>
+        <Content />
+    </Layout>
+);
 
 export default Page;
